55ec85d3748a7a4d0e152733d2f071a81f0ecf21
rafidhossain1995 <rafidhossain@pursuit.org>
2019-11-16 16:59:49 -0500

    finished q3

--- recursionlab.js
+++ recursionlab.js
@@ -14,3 +14,12 @@ const multArr = (arr) =>{
 }
 console.log(multArr([2,3,5]))
 console.log(multArr([5,5,1,2]))
+
+//  Q3
+
+const concatArr = (arr) =>{
+    if (arr.length === 0) return ""
+    let string = arr.pop()
+    return concatArr(arr) + " " + string
+}
+console.log(concatArr(['is', 'it', 'tomorrow']))
