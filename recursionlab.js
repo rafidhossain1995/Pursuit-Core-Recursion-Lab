004c72852fa4912bec8e8fff88c7e0c058f01263
rafidhossain1995 <rafidhossain@pursuit.org>
2019-11-16 20:05:43 -0500

    Finished q6

--- recursionlab.js
+++ recursionlab.js
@@ -23,3 +23,39 @@ const concatArr = (arr) =>{
     return concatArr(arr) + " " + string
 }
 console.log(concatArr(['is', 'it', 'tomorrow']))
+
+// Q4
+
+const sumEvens = (arr) =>{
+    if(arr.length === 0) return 0
+    let el = arr.pop()
+    if(el % 2 === 0){
+        return sumEvens(arr) + el
+    }else {
+        return sumEvens(arr)
+    }
+    
+}
+console.log(sumEvens([2,3,5,6,8]))
+
+// Q5
+
+const range = (num1, num2) =>{
+    let output = []
+    if(num1 > num2) return output
+    output.push(num1)
+    return output.concat(range(num1+1, num2))
+}
+console.log(range(1,10))
+
+
+//6
+
+const tripleStep = (num) =>{
+    if(num === 0) return 1
+    if(num === 1) return 1
+    if(num === 2) return 2
+    return tripleStep(num-1) + tripleStep(num-2) + tripleStep(num-3)
+
+}
+console.log(tripleStep(4))
